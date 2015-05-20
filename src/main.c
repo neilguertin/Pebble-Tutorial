bb4207035f7b61563cd34d4fa84880863b53af4c
Neil Guertin <neilguertin@gmail.com>
2015-05-20 19:25:20 -0400

    Finished first page

--- src/main.c
+++ src/main.c
@@ -2,12 +2,37 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
+static void update_time(){
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  
+  // Create a long-lived buffer
+  static char buffer[] = "00:00";
+  
+  // Write the current hours and minutes into the buffer
+  if (clock_is_24h_style() == true){
+    // Use 24hr format
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+  } else {
+    // Use 12hr format
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+  }
+  
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
+  update_time();
+}
+
+
 static void main_window_load(Window *window){
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
   
   // Improve the layout to be more like a watchface
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
@@ -34,6 +59,11 @@ static void init(){
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
   
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  // Make sure time is displayed from the start
+  update_time();
 }
 
 static void deinit(){
